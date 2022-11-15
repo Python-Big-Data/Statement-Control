673a9f55f2ade54178ab022b16fe580690292d3f
hp <sahalnurdin@gmail.com>
2022-11-15 20:12:48 +0700

    Final Commit

---
+++ Proyek Latihan 5.py
@@ -0,0 +1,14 @@
+# Buatlah program python untuk menampilkan bilangan bulat dari 0 sampai dengan 100 yang ganjil.
+# Lengkapi juga dengan menampilkan banyaknya bilangan bulat dari 0 sampai 100 yang ganjil dan tampilkan juga total
+# jumlahan seleuruh bilangan ganjil ( 1 + 3 + 5 + .. ).
+# Contoh hasil output :
+# 1
+# 3
+# 5
+# 7
+#.
+#.
+# dst
+# Banyaknya bilangan ganjil : xxx
+# Jumlah seluruh bilangan ganjil : xxx
+# Banyaknya  bilangan ganjil : xxx
