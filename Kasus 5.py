c17e5b2050618fe0c01aee811146b5a12859ca94
hp <sahalnurdin@gmail.com>
2022-11-15 17:40:06 +0700

    Final Commit

--- Kasus 5.py
+++ Kasus 5.py
@@ -1,9 +1,13 @@
 # Buatlah program untuk menghitung banyaknya bilangan bulat 1 s/d 100 yang merupakan kelipatan 3
 
 sum = 0
+sum1 = 0
 for i in range(100):
     if (i + 1) % 3 == 0:
         # Jika ini true maka akan masuk ke dalam variabel sum yang ada di bawah
         sum += 1
+    elif (i + 1) % 3 != 0:
+        sum1 += 1
 
 print("Banyaknya bilangan : " + str(sum))
+print("Banyaknya bilangan yang bukan merupakan kelipatan 3 : " + str(sum1))
