eb07e70f13749fd1bf4c9f932f300efb20973dc7
hp <sahalnurdin@gmail.com>
2022-11-15 18:57:12 +0700

    Final Commit

--- Proyek Latihan 4.py
+++ Proyek Latihan 4.py
@@ -5,9 +5,20 @@
 # Adapun rumus untuk menghitung gaji bersih adalah sebagai berikut:
 #     Gaji Kotor = Gaji Pokok  + Tunjangan Istri/Suami + Tunjangan anak
 #     Gaji Bersih = Gaji Kotor - Potongan
-# Keterengan:
+# Keterangan:
 # Perhitungan potongan dilakukan terhadap gaji kotor, yang besar % nya menyesuaikan golongan karyawan.
 
+
+# Data-Data
+A = 10000000
+B = 8500000
+C = 7000000
+D = 5000000
+
+potA = 0.025
+potB = 0.02
+potC = 0.015
+potD = 0.01
 
 # Logics
 x = 'Y'
@@ -15,20 +26,89 @@ while x == 'Y':
     kodeKaryawan = int(input('Masukkan kode karyawan      : '))
     namaKaryawan = input('Masukkan nama karyawan      : ')
     golongan = input('Masukkan golongan           : ')
-    status = input('Masukkan status (1: Menikah, 2: blm)           : ')
-    if status == 1:
-        anak = input('Masukkan jumlah anak           : ')
-    else:
+    status = input('Masukkan status (1: Menikah, 2: Belum Menikah )           : ')
+    if status == "menikah" or status == "Menikah" or status == "MENIKAH":
+        anak = int(input('Masukkan jumlah anak        : '))
+        tunjanganIstriSuami = float(0.1 * A)
+        tunjanganAnak = float(A * 0.05 * anak)
+        gajiKotor = A + tunjanganIstriSuami + tunjanganAnak
         if golongan == 'A':
             print('==============================================')
             print('STRUK RINCIAN GAJI KARYAWAN')
             print('----------------------------------------------')
             print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
             print('Golongan                    : ', golongan)
-            print('Status menikah                    : ', status)
-            print('Jumlah anak                    : ', anak)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
             print('----------------------------------------------')
             print('Gaji Pokok                  :', 'Rp', A)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potA, '(%))       :', 'Rp', float(gajiKotor * potA))
+            print('----------------------------------------------')
+            print('Gaji Bersih                 : Rp', float(gajiKotor - (gajiKotor * potA)))
+
+        elif golongan == 'B':
+            print('==============================================')
+            print('STRUK RINCIAN GAJI KARYAWAN')
+            print('----------------------------------------------')
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
+            print('----------------------------------------------')
+            print('Gaji Pokok                  :', 'Rp', B)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potB, '(%))        :', 'Rp', float(gajiKotor * potB))
+            print('----------------------------------------------')
+            print('Gaji Bersih                 : Rp', float(gajiKotor - (gajiKotor * potB)))
+        elif golongan == 'C':
+            print('==============================================')
+            print('STRUK RINCIAN GAJI KARYAWAN')
+            print('----------------------------------------------')
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
+            print('----------------------------------------------')
+            print('Gaji Pokok                  :', 'Rp', C)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potC, '(%))       :', 'Rp', float(gajiKotor * potC))
+            print('----------------------------------------------')
+            print('Gaji Bersih                 : Rp', float(gajiKotor - (gajiKotor * potC)))
+        elif golongan == 'D':
+            print('==============================================')
+            print('STRUK RINCIAN GAJI KARYAWAN')
+            print('----------------------------------------------')
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
+            print('----------------------------------------------')
+            print('Gaji Pokok                  :', 'Rp', D)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potD, '(%))        :', 'Rp', float(gajiKotor * potD))
+            print('----------------------------------------------')
+            print('Gaji Bersih                 : Rp', float(gajiKotor - (gajiKotor * potD)))
+    elif status == "belum menikah" or status == "Belum Menikah" or status == "BELUM MENIKAH":
+        anak = "nil"
+        tunjanganIstriSuami = "nil"
+        tunjanganAnak = "nil"
+        if golongan == 'A':
+            print('==============================================')
+            print('STRUK RINCIAN GAJI KARYAWAN')
+            print('----------------------------------------------')
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
+            print('----------------------------------------------')
+            print('Gaji Pokok                  :', 'Rp', A)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
             print('Potongan (', potA, '(%))       :', 'Rp', int(A * potA))
             print('----------------------------------------------')
             print('Gaji Bersih                 : Rp', int(A - (A * potA)))
@@ -37,35 +117,48 @@ while x == 'Y':
             print('==============================================')
             print('STRUK RINCIAN GAJI KARYAWAN')
             print('----------------------------------------------')
-            print('Nama Karyawan              : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
-            print('Golongan                   : ', golongan)
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
             print('----------------------------------------------')
-            print('Gaji Pokok                 :', 'Rp', B)
-            print('Potongan (', potB, '(%))       :', 'Rp', int(A * potB))
+            print('Gaji Pokok                  :', 'Rp', B)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potB, '(%))    :', 'Rp', int(B * potB))
             print('----------------------------------------------')
-            print('Gaji Bersih                : Rp', int(B - (B * potB)))
+            print('Gaji Bersih                 : Rp', int(B - (B * potB)))
         elif golongan == 'C':
             print('==============================================')
             print('STRUK RINCIAN GAJI KARYAWAN')
             print('----------------------------------------------')
-            print('Nama Karyawan              : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
-            print('Golongan                   : ', golongan)
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
             print('----------------------------------------------')
-            print('Gaji Pokok                 :', 'Rp', C)
-            print('Potongan (', potC, '(%))      :', 'Rp', int(C * potC))
+            print('Gaji Pokok                  :', 'Rp', C)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potC, '(%))    :', 'Rp',int(C * potC))
             print('----------------------------------------------')
-            print('Gaji Bersih                : Rp', int(C - (C * potC)))
+            print('Gaji Bersih                 : Rp', int(C - (C * potC)))
         elif golongan == 'D':
             print('==============================================')
             print('STRUK RINCIAN GAJI KARYAWAN')
             print('----------------------------------------------')
-            print('Nama Karyawan              : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
-            print('Golongan                   : ', golongan)
+            print('Nama Karyawan               : ', namaKaryawan, '(Kode: ', kodeKaryawan, ')')
+            print('Golongan                    : ', golongan)
+            print('Status menikah              : ', status)
+            print('Jumlah anak                 : ', anak)
             print('----------------------------------------------')
-            print('Gaji Pokok                 :', 'Rp', D)
-            print('Potongan (', potD, '(%))      :', 'Rp', int(D * potD))
+            print('Gaji Pokok                  :', 'Rp', D)
+            print('Tunjangan Istri dan Suami   :', 'Rp', tunjanganIstriSuami)
+            print('Tunjangan anak              :', 'Rp', tunjanganAnak)
+            print('Potongan (', potD, '(%))    :', 'Rp', int(D * potD))
             print('----------------------------------------------')
             print('Gaji Bersih                : Rp', int(D - (D * potD)))
+
     x = input("Apakah Anda ingin mengulanginya ? (Y/N)")
     if x == "Y" or "y":
         continue
