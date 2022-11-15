76ffce455af2c738384fbdce39ee5e32431b54ce
hp <sahalnurdin@gmail.com>
2022-11-15 20:22:27 +0700

    Final Commit

---
+++ Proyek Latihan 6.py
@@ -0,0 +1,9 @@
+# Tinjau kembali kode program pada nomor 5 di atas. Modifikasilah kode programnya sehingga diperoleh tampilan seperti
+# gambar 5.12
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+
---
+++ Proyek Latihan 7.py
@@ -0,0 +1,22 @@
+# Buatlah game tebak angka dengan seknario sebagai berikut.
+# Komputer akan memilih sebuah bilangan bulat secara random, antara 0 s/d 100
+# Bilangan tersebut tersimpan dalam memori komputer
+# Tugas pemain adalah menebak bilangan yang dipilih komputer tersebut.
+# Untuk menebak bilangan, pemain mengentri beberapa bilangan
+# Komputer memberikan respon 'Bilangan tebakan Anda terlalu besar' jika bilangan yang dientri pemain lebih besar
+# dari bilangan yang dipilih komputer atau 'Bilangan tebakan Anda terlalu kecil' jika bilangan yang di entri pemain
+# lebih kecil dari bilangan yang dipilih komputer adalah 10;
+
+# Hai.. nama saya Mr. Lappie, saya telah memilih sebuah bilangan bulat secara acak antara - s/d 100. Silakan tebak ya!!!"
+# Tebakan Anda: 4
+# hehehe.. Bilangan tebakan Anda terlalu kecil
+# Tebakan Anda: 20
+# hehehe.. Bilangan tebakan Anda terlalu kecil
+# Tebakan Anda: 15
+# hehehe.. Bilangan tebakan Anda terlalu kecil
+# Tebakan Anda: 9
+# hehehe.. Bilangan tebakan Anda terlalu kecil
+# Tebakan Anda: 10
+# hehehe.. Bilangan tebakan Anda BENAR :-)
+
+
