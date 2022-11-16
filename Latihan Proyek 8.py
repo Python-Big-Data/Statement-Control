2e3dff32a159b52d06e50ed7d5af613b167c6ffa
hp <sahalnurdin@gmail.com>
2022-11-16 07:16:27 +0700

    Final Commit

--- Latihan Proyek 8.py
+++ Latihan Proyek 8.py
@@ -17,3 +17,13 @@
 # Tebakan Anda: 10
 # Yeeee.. Bilangan tebakan Anda BENAR :-)
 # Score Anda : 92
+jawab = 'ya'
+hitung = 0
+
+while(True):
+    hitung += 1
+    jawab = input("Ulang lagi tidak? ")
+    if jawab == 'tidak':
+        break
+
+print(f"Total perulagan: {hitung}")
--- Proyek Latihan 7.py
+++ Proyek Latihan 7.py
@@ -7,17 +7,46 @@
 # dari bilangan yang dipilih komputer atau 'Bilangan tebakan Anda terlalu kecil' jika bilangan yang di entri pemain
 # lebih kecil dari bilangan yang dipilih komputer adalah 10;
 
-# Hai.. nama saya Mr. Lappie, saya telah memilih sebuah bilangan bulat secara acak antara - s/d 100.
+# Hai.. nama saya Mr. Lappie, saya telah memilih sebuah bilangan bulat secara acak antara 0 s/d 100.
 # Silakan tebak ya!!!"
 # Tebakan Anda: 4
 # hehehe.. Bilangan tebakan Anda terlalu kecil
 # Tebakan Anda: 20
-# hehehe.. Bilangan tebakan Anda terlalu kecil
+# hehehe.. Bilangan tebakan Anda terlalu besar
 # Tebakan Anda: 15
-# hehehe.. Bilangan tebakan Anda terlalu kecil
+# hehehe.. Bilangan tebakan Anda terlalu besar
 # Tebakan Anda: 9
 # hehehe.. Bilangan tebakan Anda terlalu kecil
 # Tebakan Anda: 10
 # yeeee.. Bilangan tebakan Anda BENAR :-)
+
+# Game tebakan
+namaPemain = input("Masukkan nama pemain: ")
+print("Hai.. nama saya", namaPemain, ", saya telah memilih sebuah bilangan bulat secara acak antara 0 s/d 100. ")
+
+print("Silakan tebak ya!!!")
+
+jawaban = 10
+while True:
+    tebakan = int(input("Tebakan Anda: "))
+    jawaban = 10
+    if tebakan == 10:
+        print("yeeee.. Bilangan tebakan Anda BENAR :-)")
+        break
+    elif tebakan <= 10:
+        print("hehehe.. Bilangan tebakan Anda terlalu kecil")
+        tebakan != 10
+    else:
+        print("hehehe.. Bilangan tebakan Anda terlalu besar")
+        tebakan != 10
+
+
+
+
+
+
+
+
+
 
 
